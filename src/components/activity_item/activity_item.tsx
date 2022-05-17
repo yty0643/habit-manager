66657889de875e3c3b13a204c50dbcc7e881f88d
yty0643 <80657819+yty0643@users.noreply.github.com>
2022-05-17 18:38:02 +0900

    handle error

--- src/components/activity_item/activity_item.tsx
+++ src/components/activity_item/activity_item.tsx
@@ -4,15 +4,15 @@ import styles from './activity_item.module.css';
 import { faCalendarCheck } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const ActivityItem = ({ isDark, date, item }: { isDark:boolean, date: string, item: IBox }) => {
+const ActivityItem = ({ isDark, date, item }: { isDark: boolean, date: string, item: IBox }) => {
     return (
         <div className={`${styles.section} ${isDark && styles.dark}`}>
-            <p className={`${styles.date} ${isDark && styles.dark}`}>
+            <div className={`${styles.date} ${isDark && styles.dark}`}>
                 <div className={styles.icon}>
                     <FontAwesomeIcon icon={faCalendarCheck} />
                 </div>
                 {date}
-            </p>    
+            </div>
             <div className={styles.description}>
                 recorded
                 <p className={styles.value}>{(item.totalTime / 1000 / 3600).toFixed(3)}</p>
@@ -22,10 +22,10 @@ const ActivityItem = ({ isDark, date, item }: { isDark:boolean, date: string, it
             </div>
             <div className={styles.box}>
                 <div className={styles.start}>
-                    <p>{Object.values(item.habitTime).map(item => (<p>{item[0]}</p>))}</p>
+                    {Object.values(item.habitTime).map((item, index) => (<p key={index}>{item[0]}</p>))}
                 </div>
                 <div className={styles.end}>
-                    {Object.values(item.habitTime).map(item => (<p>- {item[1]}</p>))}
+                    {Object.values(item.habitTime).map((item, index) => (<p key={index}>- {item[1]}</p>))}
                 </div>
             </div>
         </div>
